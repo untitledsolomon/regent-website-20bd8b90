bedda13f87469c1fdd8e7ffed210c7322d19b5d9
Solomon <solomonjohnc818@gmail.com>
2026-03-12 21:30:27 +0300

    Fix: Make dist folder in vervcel if null

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import sitemap from "vite-plugin-sitemap";
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -14,16 +14,20 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
+    // Ensure dist exists first
+    {
+      name: "ensure-dist",
+      buildStart() {
+        if (!fs.existsSync("dist")) fs.mkdirSync("dist");
+      },
+    },
     sitemap({
       hostname: "https://www.regentplatform.com",
       outDir: "dist",
-      // type-safe robots array
       robots: [
         {
           userAgent: "*",
           allow: "/",
-          // disallow: "/private", // optional
-          // crawlDelay: 5, // optional
         },
       ],
     }),
