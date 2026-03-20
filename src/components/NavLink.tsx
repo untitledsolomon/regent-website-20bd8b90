ee28a39e794995a5c8ae7c759ff5277e308f4788
Solomon <solomonjohnc818@gmail.com>
2026-03-20 15:36:48 +0300

    fix: remove React Router ClassNameValue type from NavLink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -5,10 +5,6 @@ import { usePathname } from "next/navigation";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
-type ClassNameValue =
-  | string
-  | ((args: { isActive: boolean; isPending: boolean }) => string | undefined);
-
 interface NavLinkCompatProps
   extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "className" | "href"> {
   to: string;
@@ -23,17 +19,12 @@ const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
     const isActive = pathname === to;
     const isPending = false;
 
-    const resolvedClassName =
-      typeof className === "function"
-        ? (className as ClassNameValue)({ isActive, isPending })
-        : className;
-
     return (
       <Link href={to} legacyBehavior>
         <a
           ref={ref}
           className={cn(
-            resolvedClassName,
+            className,
             isActive && activeClassName,
             isPending && pendingClassName,
           )}
